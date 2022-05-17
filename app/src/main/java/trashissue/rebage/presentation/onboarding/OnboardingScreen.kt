99cd8d8d752e89537abf1fafac9c4d0fddae2dc6
HafizhDaffa <hafizhdaffa050901@gmail.com>
2022-05-17 11:46:46 +0700

    wip Onboarding Screen

--- app/src/main/java/trashissue/rebage/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/trashissue/rebage/presentation/onboarding/OnboardingScreen.kt
@@ -1,2 +1,42 @@
 package trashissue.rebage.presentation.onboarding
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import trashissue.rebage.R
+
+@Composable
+fun OnboardingScreen() {
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_logohorizontal),
+                contentDescription = null
+            )
+            TextButton(
+                onClick = {
+
+                }
+            ) {
+                TextButton(
+                    onClick = {
+
+                    }
+                ) {
+                    Text(text = stringResource(R.string.text_skip))
+                }
+            }
+        }
+    }
+}
