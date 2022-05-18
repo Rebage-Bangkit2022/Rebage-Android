23283ca5509c6a12b65ccd5be083d1e521493b64
bagus2x <dutapanggulu@gmail.com>
2022-05-18 08:53:23 +0700

    create Page, Pages (Pager), and TopBar for OnboardingScreen

---
+++ app/src/main/java/trashissue/rebage/presentation/onboarding/component/Page.kt
@@ -0,0 +1,67 @@
+package trashissue.rebage.presentation.onboarding.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import trashissue.rebage.R
+import trashissue.rebage.presentation.theme.RebageTheme
+
+@Composable
+fun Page(
+    painter: Painter,
+    title: String,
+    subTitle: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = title,
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .widthIn(300.dp, 400.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = subTitle,
+            style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PagePreview() {
+    RebageTheme {
+        Page(
+            painter = painterResource(R.drawable.ic_onboarding1),
+            title = "Test one two three four five ten eleven twelve",
+            subTitle = "Test one two three four five ten eleven twelve Test one two three four five ten eleven twelve"
+        )
+    }
+}
---
+++ app/src/main/java/trashissue/rebage/presentation/onboarding/component/Pages.kt
@@ -0,0 +1,61 @@
+package trashissue.rebage.presentation.onboarding.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+import trashissue.rebage.R
+import trashissue.rebage.presentation.theme.RebageTheme
+
+val OnboardingContent = listOf(
+    Triple(
+        R.drawable.ic_onboarding1,
+        R.string.title_onboarding1,
+        R.string.subtitle_onboarding1,
+    ),
+    Triple(
+        R.drawable.ic_onboarding2,
+        R.string.title_onboarding2,
+        R.string.subtitle_onboarding2,
+    ),
+    Triple(
+        R.drawable.ic_onboarding3,
+        R.string.title_onboarding3,
+        R.string.subtitle_onboarding3,
+    )
+)
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun Pages(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState = rememberPagerState(),
+) {
+    HorizontalPager(
+        modifier = modifier,
+        count = OnboardingContent.size,
+        state = pagerState
+    ) { page ->
+        val (illustration, title, subtitle) = OnboardingContent[page]
+
+        Page(
+            painter = painterResource(illustration),
+            title = stringResource(title),
+            subTitle = stringResource(subtitle)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+@OptIn(ExperimentalPagerApi::class)
+fun PagesPreview() {
+    RebageTheme {
+        Pages(pagerState = rememberPagerState())
+    }
+}
