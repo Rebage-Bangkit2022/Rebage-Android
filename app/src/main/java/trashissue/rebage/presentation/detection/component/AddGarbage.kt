39c1c4e3eb62f1b711b0847af3d1e0fba7b67821
bagus2x <dutapanggulu@gmail.com>
2022-05-21 07:56:10 +0700

    update button in DetectionScreen

--- app/src/main/java/trashissue/rebage/presentation/detection/component/AddGarbage.kt
+++ app/src/main/java/trashissue/rebage/presentation/detection/component/AddGarbage.kt
@@ -1,48 +1,76 @@
 package trashissue.rebage.presentation.detection.component
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 
-
 @Composable
 fun AddGarbage(
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .clip(MaterialTheme.shapes.medium)
-            .border(
-                width = 1.dp,
-                color = MaterialTheme.colors.primary,
-                shape = MaterialTheme.shapes.medium
-            )
-            .padding(12.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-            DropdownGarbage()
-            Icon(
-                imageVector = Icons.Outlined.Delete,
-                contentDescription = "Delete"
-            )
-        }
-        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-            Text(
-                text = "Jumlah Deteksi",
-                modifier = Modifier.weight(1F),
-                style = MaterialTheme.typography.caption
-            )
-            Counter()
+    var addNewGarbage by rememberSaveable { mutableStateOf(false) }
+
+    Crossfade(addNewGarbage) {
+        if (it) {
+            Column(
+                modifier = modifier
+                    .clip(MaterialTheme.shapes.medium)
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colors.primary,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .padding(12.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                    DropdownGarbage()
+                    Icon(
+                        modifier = Modifier.clickable {
+                            addNewGarbage = false
+                        },
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = "Delete"
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Jumlah Deteksi",
+                        modifier = Modifier.weight(1F),
+                        style = MaterialTheme.typography.caption
+                    )
+                    Counter()
+                }
+            }
+        } else {
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    addNewGarbage = true
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "Tambah")
+            }
         }
     }
 }
