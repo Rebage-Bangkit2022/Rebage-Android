8f45b747a8a71542066f13f267ddb2343e899bb0
HafizhDaffa <hafizhdaffa050901@gmail.com>
2022-05-21 11:08:38 +0700

    Update color for pie chart

--- app/src/main/java/trashissue/rebage/presentation/theme/Color.kt
+++ app/src/main/java/trashissue/rebage/presentation/theme/Color.kt
@@ -8,3 +8,15 @@ val ForestGreen700 = Color(0xFF437A53)
 val Jasmine200 = Color(0xFFFFFFAB)
 val Jasmine500 = Color(0xFFf9DE7A)
 val Jasmine700 = Color(0xFFC4AC4B)
+val PeachChart = Color(0xFFF2D08F)
+val TealChart = Color(0xFF8FF2AB)
+val JordyChart = Color(0xFF8FC2F2)
+val PastelChart = Color(0xFFA38FF2)
+val BlueGChart = Color(0xFF5989D1)
+val PistachioChart = Color(0xFF97C573)
+val TulipChart = Color(0xFFF28F8F)
+val SiennaChart = Color(0xFFED724B)
+
+
+
+
