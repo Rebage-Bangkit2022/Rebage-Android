3b18d3fe32b7777714347b1b540a6bcc6238f832
bagus2x <dutapanggulu@gmail.com>
2022-05-22 08:44:22 +0700

    wip price screen

--- app/src/main/java/trashissue/rebage/presentation/price/PriceScreen.kt
+++ app/src/main/java/trashissue/rebage/presentation/price/PriceScreen.kt
@@ -1,7 +1,44 @@
 package trashissue.rebage.presentation.price
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import trashissue.rebage.R
+import trashissue.rebage.presentation.common.statusBarsPaddingWithColor
 
 @Composable
 fun PriceScreen() {
+    Scaffold(
+        modifier = Modifier
+            .statusBarsPaddingWithColor()
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar {
+                Text(
+                    text = stringResource(R.string.text_price),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colors.onPrimary
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+
+        }
+    }
 }
