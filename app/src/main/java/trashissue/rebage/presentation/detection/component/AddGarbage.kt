12bf8d8376ecfccda180b8b4a589404e3f24e2ae
bagus2x <dutapanggulu@gmail.com>
2022-05-23 16:23:06 +0700

    update design

--- app/src/main/java/trashissue/rebage/presentation/detection/component/AddGarbage.kt
+++ app/src/main/java/trashissue/rebage/presentation/detection/component/AddGarbage.kt
@@ -1,16 +1,16 @@
 package trashissue.rebage.presentation.detection.component
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import trashissue.rebage.presentation.common.noRippleClickable
+import trashissue.rebage.R
+
+val DefaultButtonContentPadding = PaddingValues(horizontal = 4.dp, vertical = 2.dp)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -18,38 +18,41 @@ fun AddGarbage(
     modifier: Modifier = Modifier,
     onCancel: () -> Unit
 ) {
-
     Card {
         Column(
             modifier = modifier
                 .padding(12.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                DropdownGarbage()
-                Icon(
-                    modifier = Modifier.noRippleClickable(onClick = onCancel),
-                    imageVector = Icons.Outlined.Delete,
-                    contentDescription = "Delete"
-                )
-            }
+            DropdownGarbage()
             Row(
                 horizontalArrangement = Arrangement.spacedBy(4.dp),
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.align(Alignment.End)
             ) {
-                Text(
-                    text = "Jumlah Deteksi",
-                    modifier = Modifier.weight(1F),
-                    style = MaterialTheme.typography.bodyMedium
-                )
+                TextButton(
+                    onClick = onCancel,
+                    contentPadding = DefaultButtonContentPadding
+                ) {
+                    Text(
+                        text = stringResource(R.string.text_cancel),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+                TextButton(
+                    onClick = onCancel,
+                    contentPadding = DefaultButtonContentPadding
+                ) {
+                    Text(text = stringResource(R.string.text_save))
+                }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RowScope.DropdownGarbage(
+fun DropdownGarbage(
     modifier: Modifier = Modifier
 ) {
     val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
@@ -58,7 +61,7 @@ fun RowScope.DropdownGarbage(
     val focusManager = LocalFocusManager.current
 
     ExposedDropdownMenuBox(
-        modifier = modifier.weight(1F),
+        modifier = modifier,
         expanded = expanded,
         onExpandedChange = {
             expanded = !expanded
@@ -69,7 +72,7 @@ fun RowScope.DropdownGarbage(
             value = selectedOptionText,
             onValueChange = { },
             label = {
-                Text("Pilih Sampah")
+                Text(stringResource(R.string.text_choose_item))
             },
             trailingIcon = {
                 ExposedDropdownMenuDefaults.TrailingIcon(
