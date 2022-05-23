8ba612a808f786a09ed94fed42299fa5530da7c8
bagus2x <dutapanggulu@gmail.com>
2022-05-23 16:22:22 +0700

    change padding

--- app/src/main/java/trashissue/rebage/presentation/detection/component/Counter.kt
+++ app/src/main/java/trashissue/rebage/presentation/detection/component/Counter.kt
@@ -1,15 +1,21 @@
 package trashissue.rebage.presentation.detection.component
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Remove
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import trashissue.rebage.presentation.common.noRippleClickable
 
 @Composable
 fun Counter(
@@ -24,15 +30,33 @@ fun Counter(
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         Icon(
-            imageVector = Icons.Outlined.Add,
+            imageVector = Icons.Outlined.Remove,
             contentDescription = null,
-            modifier = Modifier.noRippleClickable(onClick = onClickDecrement)
+            modifier = Modifier
+                .size(16.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 1.dp,
+                    shape = CircleShape,
+                    color = MaterialTheme.colorScheme.onSurface.copy(0.2F)
+                )
+                .clickable(onClick = onClickDecrement),
+            tint = MaterialTheme.colorScheme.onSurface
         )
         text()
         Icon(
             imageVector = Icons.Outlined.Add,
             contentDescription = null,
-            modifier = Modifier.noRippleClickable(onClick = onClickIncrement)
+            modifier = Modifier
+                .size(16.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 1.dp,
+                    shape = CircleShape,
+                    color = MaterialTheme.colorScheme.onSurface.copy(0.2F)
+                )
+                .clickable(onClick = onClickIncrement),
+            tint = MaterialTheme.colorScheme.onSurface
         )
     }
 }
