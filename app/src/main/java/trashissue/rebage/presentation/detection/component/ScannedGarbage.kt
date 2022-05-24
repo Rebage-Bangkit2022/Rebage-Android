8b4b1ff94249843fc05fbd7bc00009e0916855cd
bagus2x <dutapanggulu@gmail.com>
2022-05-24 07:43:05 +0700

    remove unused annotation

--- app/src/main/java/trashissue/rebage/presentation/detection/component/ScannedGarbage.kt
+++ app/src/main/java/trashissue/rebage/presentation/detection/component/ScannedGarbage.kt
@@ -1,7 +1,5 @@
 package trashissue.rebage.presentation.detection.component
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -15,14 +13,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import trashissue.rebage.R
 import trashissue.rebage.presentation.theme3.RebageTheme3
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScannedGarbage(
     modifier: Modifier = Modifier
@@ -69,23 +65,11 @@ fun ScannedGarbage(
 
                         if (editMode) {
                             var item by rememberSaveable { mutableStateOf(0) }
-
-                            Counter(
-                                onClickDecrement = { item-- },
+                            AnimatedCounter(
+                                value = item,
+                                onClickDecrement = { if (item != 0) item-- },
                                 onClickIncrement = { item++ }
-                            ) {
-                                AnimatedContent(item) {
-                                    Text(
-                                        text = "$item",
-                                        style = MaterialTheme.typography.titleMedium,
-                                        modifier = Modifier
-                                            .padding(horizontal = 12.dp)
-                                            .widthIn(min = 16.dp),
-                                        textAlign = TextAlign.Center,
-                                        textDecoration = TextDecoration.Underline
-                                    )
-                                }
-                            }
+                            )
                         } else {
                             Text(
                                 text = "3 Items",
