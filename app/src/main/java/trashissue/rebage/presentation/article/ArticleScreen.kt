a9c9cb707b699b624066c71d67e0476b0b2306ee
bagus2x <dutapanggulu@gmail.com>
2022-05-25 15:14:36 +0700

    add article screen

--- app/src/main/java/trashissue/rebage/presentation/article/ArticleScreen.kt
+++ app/src/main/java/trashissue/rebage/presentation/article/ArticleScreen.kt
@@ -1,16 +1,20 @@
 package trashissue.rebage.presentation.article
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBackIos
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import trashissue.rebage.R
+import trashissue.rebage.presentation.article.component.Images
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,7 +27,7 @@ fun ArticleScreen(
         topBar = {
             SmallTopAppBar(
                 title = {
-                    Text(text = "Title $articleId")
+                    Text(text = stringResource(R.string.text_recycle))
                 },
                 navigationIcon = {
                     IconButton(
@@ -40,8 +44,40 @@ fun ArticleScreen(
             )
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "DETAIL articleid $articleId")
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(scrollState)
+        ) {
+            Images(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
+                text = "Title Title Title Title Title Title Title Title Title Title Title Title",
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+                text = "Author name",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = "24 November 2022",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "Body Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body  Body ",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Justify
+            )
         }
     }
 }
