02a36a33fa6f6c632f5aa21078d301ff3848c3a1
bagus2x <dutapanggulu@gmail.com>
2022-05-27 18:39:54 +0700

    add fuction deleteUser

--- app/src/main/java/trashissue/rebage/data/local/datastore/UserPreferences.kt
+++ app/src/main/java/trashissue/rebage/data/local/datastore/UserPreferences.kt
@@ -14,18 +14,28 @@ class UserPreferences(
     suspend fun saveUser(user: UserEntity) {
         dataStore.edit { preferences ->
             preferences[KEY_USER_ID] = user.id
-            preferences[KEY_USER_PHOTO] = user.photo
             preferences[KEY_USER_EMAIL] = user.email
             preferences[KEY_USER_NAME] = user.name
+            if (user.photo != null) preferences[KEY_USER_PHOTO] = user.photo
             preferences[KEY_USER_TOKEN] = user.token
+        }
+    }
+
+    suspend fun deleteUser() {
+        dataStore.edit { mutablePreferences ->
+            mutablePreferences.remove(KEY_USER_ID)
+            mutablePreferences.remove(KEY_USER_NAME)
+            mutablePreferences.remove(KEY_USER_EMAIL)
+            mutablePreferences.remove(KEY_USER_PHOTO)
+            mutablePreferences.remove(KEY_USER_TOKEN)
         }
     }
 
     companion object {
         private val KEY_USER_ID = intPreferencesKey("user_id")
-        private val KEY_USER_PHOTO = stringPreferencesKey("user_name")
-        private val KEY_USER_EMAIL = stringPreferencesKey("user_name")
         private val KEY_USER_NAME = stringPreferencesKey("user_name")
+        private val KEY_USER_EMAIL = stringPreferencesKey("user_email")
+        private val KEY_USER_PHOTO = stringPreferencesKey("user_photo")
         private val KEY_USER_TOKEN = stringPreferencesKey("user_token")
     }
 }
