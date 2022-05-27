86771becd4d77821744d63552f95bc603632a266
bagus2x <dutapanggulu@gmail.com>
2022-05-27 18:44:35 +0700

    change photo to nullable string

--- app/src/main/java/trashissue/rebage/domain/model/User.kt
+++ app/src/main/java/trashissue/rebage/domain/model/User.kt
@@ -2,8 +2,8 @@ package trashissue.rebage.domain.model
 
 data class User(
     val id: Int,
-    val photo: String,
     val name: String,
     val email: String,
+    val photo: String?,
     val token: String
 )
