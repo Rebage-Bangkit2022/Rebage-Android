44d9cb886082d2cfd8a7a7aa6c3db450654ad7b4
bagus2x <dutapanggulu@gmail.com>
2022-06-02 18:36:25 +0700

    create function to map detected garbage by label

--- app/src/main/java/trashissue/rebage/domain/model/DetectedGarbage.kt
+++ app/src/main/java/trashissue/rebage/domain/model/DetectedGarbage.kt
@@ -1,5 +1,7 @@
 package trashissue.rebage.domain.model
 
+import java.util.*
+
 data class DetectedGarbage(
     val image: String,
     val detected: List<Detected>
@@ -10,4 +12,30 @@ data class DetectedGarbage(
         val boundingBox: List<Float>,
         val score: Float
     )
+
+    data class Group(
+        val image: String,
+        val label: String,
+        val boundingBoxes: List<List<Float>>,
+        val scores: List<Float>,
+        val date: String,
+        val total: Int
+    )
+
+    fun groupByLabel(): List<Group> {
+        return detected
+            .groupBy { it.label }
+            .map { map ->
+                Group(
+                    image = image,
+                    label = map.key,
+                    boundingBoxes = map.value.map { it.boundingBox },
+                    scores = map.value.map { it.score },
+                    date = Date().toString(),
+                    total = map.value.size
+                )
+            }
+    }
 }
+
+
