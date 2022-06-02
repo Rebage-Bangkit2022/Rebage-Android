4d5d31d18eb23829f8a9fc11926e46bc97a3c071
bagus2x <dutapanggulu@gmail.com>
2022-06-02 18:26:53 +0700

    delete ununused new line

--- app/src/main/java/trashissue/rebage/data/remote/payload/Api.kt
+++ app/src/main/java/trashissue/rebage/data/remote/payload/Api.kt
@@ -2,7 +2,6 @@ package trashissue.rebage.data.remote.payload
 
 import com.google.gson.annotations.SerializedName
 
-
 data class Api<T>(
     @field:SerializedName("success")
     val success: Boolean,
