7b74c02dfb0c9c9c5f223d46485180490a294a54
bagus2x <dutapanggulu@gmail.com>
2022-06-04 15:13:35 +0700

    update paramater for getArticles

--- app/src/main/java/trashissue/rebage/data/DefaultArticleRepository.kt
+++ app/src/main/java/trashissue/rebage/data/DefaultArticleRepository.kt
@@ -1,7 +1,5 @@
 package trashissue.rebage.data
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import trashissue.rebage.data.mapper.asModel
 import trashissue.rebage.data.remote.ArticleRemoteDataSource
 import trashissue.rebage.domain.model.Article
@@ -11,14 +9,17 @@ class DefaultArticleRepository(
     private val articleRemoteDataSource: ArticleRemoteDataSource
 ) : ArticleRepository {
 
-    override fun getArticles(category: String?, page: Int, size: Int): Flow<List<Article>> {
-        return flow {
-            val res = articleRemoteDataSource.getArticles(category, page, size)
-            emit(res.map { it.asModel() })
-        }
+    override suspend fun getArticles(
+        category: String?,
+        garbageCategory: String?,
+        page: Int,
+        size: Int
+    ): List<Article> {
+        return articleRemoteDataSource.getArticles(category, garbageCategory, page, size)
+            .map { it.asModel() }
     }
 
-    override suspend fun getArticle(articleId: Int): Article{
+    override suspend fun getArticle(articleId: Int): Article {
         return articleRemoteDataSource.getArticle(articleId).asModel()
     }
 }
