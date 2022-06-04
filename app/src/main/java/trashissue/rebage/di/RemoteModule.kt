9d76ac6a609b36a5a136eaa7142327fa3765b684
bagus2x <dutapanggulu@gmail.com>
2022-06-04 15:18:26 +0700

    provide datasource dependency injection

--- app/src/main/java/trashissue/rebage/di/RemoteModule.kt
+++ app/src/main/java/trashissue/rebage/di/RemoteModule.kt
@@ -10,11 +10,14 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
 import trashissue.rebage.BuildConfig
 import trashissue.rebage.data.remote.ArticleRemoteDataSource
+import trashissue.rebage.data.remote.DetectionRemoteDataSource
 import trashissue.rebage.data.remote.GarbageRemoteDataSource
 import trashissue.rebage.data.remote.UserRemoteDataSource
 import trashissue.rebage.data.remote.service.ArticleService
+import trashissue.rebage.data.remote.service.DetectionService
 import trashissue.rebage.data.remote.service.GarbageService
 import trashissue.rebage.data.remote.service.UserService
 import java.util.concurrent.TimeUnit
@@ -56,21 +59,28 @@ object RemoteModule {
     @Provides
     @Singleton
     fun provideUserRemoteDataSource(retrofit: Retrofit): UserRemoteDataSource {
-        val userService = retrofit.create(UserService::class.java)
-        return UserRemoteDataSource(userService = userService)
+        val userService = retrofit.create<UserService>()
+        return UserRemoteDataSource(userService)
     }
 
     @Provides
     @Singleton
-    fun provideGarbageRemoteDataSource(retrofit: Retrofit): GarbageRemoteDataSource {
-        val garbageService = retrofit.create(GarbageService::class.java)
-        return GarbageRemoteDataSource(garbageService = garbageService)
+    fun provideDetectionRemoteDataSource(retrofit: Retrofit): DetectionRemoteDataSource {
+        val detectionService = retrofit.create<DetectionService>()
+        return DetectionRemoteDataSource(detectionService)
     }
 
     @Provides
     @Singleton
     fun provideArticleRemoteDataSource(retrofit: Retrofit): ArticleRemoteDataSource {
-        val articleService = retrofit.create(ArticleService::class.java)
-        return ArticleRemoteDataSource(articleService = articleService)
+        val articleService = retrofit.create<ArticleService>()
+        return ArticleRemoteDataSource(articleService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGarbageRemoteDataSource(retrofit: Retrofit): GarbageRemoteDataSource {
+        val garbageService = retrofit.create<GarbageService>()
+        return GarbageRemoteDataSource(garbageService)
     }
 }
