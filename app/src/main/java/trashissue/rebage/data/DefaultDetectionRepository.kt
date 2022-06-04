cebbc4287fdf273ae6e682fcc14b8ba849cff6fc
bagus2x <dutapanggulu@gmail.com>
2022-06-04 15:15:59 +0700

    create DefaultDetectionRepository

---
+++ app/src/main/java/trashissue/rebage/data/DefaultDetectionRepository.kt
@@ -0,0 +1,39 @@
+package trashissue.rebage.data
+
+import trashissue.rebage.data.mapper.asModel
+import trashissue.rebage.data.remote.DetectionRemoteDataSource
+import trashissue.rebage.data.remote.payload.UpdateDetectionRequest
+import trashissue.rebage.domain.model.Detection
+import trashissue.rebage.domain.repository.DetectionRepository
+import java.io.File
+
+class DefaultDetectionRepository(
+    private val detectionRemoteDataSource: DetectionRemoteDataSource
+) : DetectionRepository {
+
+    override suspend fun detect(token: String, file: File): List<Detection> {
+        val res = detectionRemoteDataSource.detect(token, file)
+        return res.map { it.asModel() }
+    }
+
+    override suspend fun getDetectedGarbage(token: String): List<Detection> {
+        val res = detectionRemoteDataSource.getDetectedGarbage(token)
+        return res.map { it.asModel() }
+    }
+
+    override suspend fun getDetectedGarbage(token: String, id: Int): Detection {
+        val res = detectionRemoteDataSource.getDetectedGarbage(token, id)
+        return res.asModel()
+    }
+
+    override suspend fun update(token: String, id: Int, total: Int): Detection {
+        val req = UpdateDetectionRequest(id = id, total = total)
+        val res = detectionRemoteDataSource.update(token, req)
+        return res.asModel()
+    }
+
+    override suspend fun delete(token: String, id: Int): Detection {
+        val res = detectionRemoteDataSource.delete(token, id)
+        return res.asModel()
+    }
+}
