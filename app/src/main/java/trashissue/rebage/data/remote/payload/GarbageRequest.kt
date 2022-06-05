3601dfd8be9f8cef2465bb88d25775db0b03ccb5
bagus2x <dutapanggulu@gmail.com>
2022-06-05 12:45:41 +0700

    delete unused files

--- app/src/main/java/trashissue/rebage/data/remote/payload/GarbageRequest.kt
+++
@@ -1,8 +0,0 @@
-package trashissue.rebage.data.remote.payload
-
-import com.google.gson.annotations.SerializedName
-
-data class GarbageRequest(
-    @field:SerializedName("imageBase64")
-    val imageBase64: String
-)
