2a85d27bd9cd4a6f42b86bf97b92682bb1e7d0c1
bagus2x <dutapanggulu@gmail.com>
2022-06-05 12:56:14 +0700

    delete modifier statusBarsPaddingWithColor

--- app/src/main/java/trashissue/rebage/presentation/common/Modifier.kt
+++ app/src/main/java/trashissue/rebage/presentation/common/Modifier.kt
@@ -2,18 +2,9 @@ package trashissue.rebage.presentation.common
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 
 inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
     clickable(
@@ -21,19 +12,4 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
         interactionSource = remember { MutableInteractionSource() }) {
         onClick()
     }
-}
-
-fun Modifier.statusBarsPaddingWithColor(color: Color? = null): Modifier = composed {
-    val density = LocalDensity.current
-    val height = WindowInsets.statusBars.getTop(density).toFloat()
-    val darkMode = isSystemInDarkTheme()
-    val backgroundColor = color ?: MaterialTheme.colorScheme.primary
-
-    drawBehind {
-        val width = size.width
-        drawRect(
-            color = if (darkMode) Color.Transparent else backgroundColor,
-            size = Size(width, height)
-        )
-    }.statusBarsPadding()
 }
