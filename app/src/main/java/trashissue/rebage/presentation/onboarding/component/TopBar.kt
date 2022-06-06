262eaf26308ef75db6d7774b31edfdc37ff594e0
bagus2x <dutapanggulu@gmail.com>
2022-06-06 09:58:35 +0700

    update import

--- app/src/main/java/trashissue/rebage/presentation/onboarding/component/TopBar.kt
+++ app/src/main/java/trashissue/rebage/presentation/onboarding/component/TopBar.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import trashissue.rebage.R
-import trashissue.rebage.presentation.common.noRippleClickable
+import trashissue.rebage.presentation.common.component.noRippleClickable
 import trashissue.rebage.presentation.theme3.RebageTheme3
 
 @Composable
