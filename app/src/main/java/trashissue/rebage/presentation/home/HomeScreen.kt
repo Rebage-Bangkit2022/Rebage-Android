7803c821ed90c8ea6300a8950cafd8d1c62e6b51
bagus2x <dutapanggulu@gmail.com>
2022-06-06 09:57:49 +0700

    make screen more stateless and connect with domain layer

--- app/src/main/java/trashissue/rebage/presentation/home/HomeScreen.kt
+++ app/src/main/java/trashissue/rebage/presentation/home/HomeScreen.kt
@@ -1,99 +1,136 @@
 package trashissue.rebage.presentation.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import trashissue.rebage.R
-import trashissue.rebage.presentation.common.statusBarsPaddingWithColor
+import trashissue.rebage.domain.model.Article
+import trashissue.rebage.domain.model.DetectionStatistic
 import trashissue.rebage.presentation.home.component.Articles
-import trashissue.rebage.presentation.home.component.ChartData
 import trashissue.rebage.presentation.home.component.Header
 import trashissue.rebage.presentation.main.Route
-import trashissue.rebage.presentation.theme3.*
-
-val DummyGarbageStat = listOf(
-    ChartData("Plastik", SiennaChart, 4.0),
-    ChartData("Kardus", BlueGChart, 3.0),
-    ChartData("Bebek", PastelChart, 16.0),
-    ChartData("Ayam", TealChart, 12.0),
-    ChartData("Plastik", JordyChart, 14.0),
-    ChartData("Kardus", Color.Gray, 5.0),
-    ChartData("Bebek", PistachioChart, 6.0),
-    ChartData("Ayam", TulipChart, 8.0),
-    ChartData("Plastik", PeachChart, 9.0),
-    ChartData("Kardus", ForestGreen, 20.0),
-    ChartData("Bebek", ForestGreen, 11.0),
-    ChartData("Ayam", SiennaChart, 13.0),
-)
+import trashissue.rebage.presentation.theme3.RebageTheme3
 
 @Composable
 fun HomeScreen(
     navController: NavHostController,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPaddingWithColor()
-            .verticalScroll(rememberScrollState())
-    ) {
+    val snackbarHostState = remember { SnackbarHostState() }
 
-        val isLight = MaterialTheme.colorScheme.isLight()
-        val systemUiController = rememberSystemUiController()
+    LaunchedEffect(Unit) {
+        viewModel.snackbar.collectLatest(snackbarHostState::showSnackbar)
+    }
 
-        DisposableEffect(isLight) {
-            systemUiController.setStatusBarColor(Color.Transparent, darkIcons = false)
-
-            onDispose {
-                systemUiController.setStatusBarColor(Color.Transparent, darkIcons = isLight)
-            }
+    HomeScreen(
+        snackbarHostState = snackbarHostState,
+        usernameState = viewModel.username,
+        allArticlesState = viewModel.allArticles,
+        articlesReduce = viewModel.articlesReduce,
+        articlesReuse = viewModel.articleReuse,
+        statsState = viewModel.stats,
+        onNavigateToDetailArticle = { id ->
+            navController.navigate(Route.Article(id))
         }
+    )
+}
 
-        Header(modifier = Modifier.padding(horizontal = 16.dp, vertical = 32.dp))
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    snackbarHostState: SnackbarHostState,
+    usernameState: StateFlow<String?>,
+    allArticlesState: StateFlow<List<Article>>,
+    articlesReduce: StateFlow<List<Article>>,
+    articlesReuse: StateFlow<List<Article>>,
+    statsState: StateFlow<List<DetectionStatistic>>,
+    onNavigateToDetailArticle: (Int) -> Unit,
+) {
 
-        val allArticlesResult by viewModel.allArticles.collectAsState()
-        val handicraftsResult by viewModel.handicrafts.collectAsState()
-        val reduceResult by viewModel.reduce.collectAsState()
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        ) {
 
-        Articles(
-            label = stringResource(R.string.text_all_articles),
-            articles = allArticlesResult,
-            onClickArticle = { articleId ->
-                navController.navigate(Route.Article(articleId))
+            val isLight = MaterialTheme.colorScheme.isLight()
+            val systemUiController = rememberSystemUiController()
+
+            DisposableEffect(isLight) {
+                systemUiController.setStatusBarColor(Color.Transparent, darkIcons = false)
+                onDispose {
+                    systemUiController.setStatusBarColor(Color.Transparent, darkIcons = isLight)
+                }
             }
-        )
-        Articles(
-            label = stringResource(R.string.text_handicraft_product),
-            articles = handicraftsResult,
-            onClickArticle = { articleId ->
-                navController.navigate(Route.Article(articleId))
-            }
-        )
-        Articles(
-            label = stringResource(R.string.text_reduce),
-            articles = reduceResult,
-            onClickArticle = { articleId ->
-                navController.navigate(Route.Article(articleId))
-            }
-        )
+
+            val username by usernameState.collectAsState()
+            val stats by statsState.collectAsState()
+            val allArticles by allArticlesState.collectAsState()
+            val reuse by articlesReuse.collectAsState()
+            val reduce by articlesReduce.collectAsState()
+
+            Header(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .padding(horizontal = 16.dp, vertical = 32.dp),
+                name = username,
+                stats = stats
+            )
+            Articles(
+                label = stringResource(R.string.text_all_articles),
+                articles = allArticles,
+                onClickArticle = onNavigateToDetailArticle
+            )
+            Articles(
+                label = stringResource(R.string.text_handicraft_product),
+                articles = reuse,
+                onClickArticle = onNavigateToDetailArticle
+            )
+            Articles(
+                label = stringResource(R.string.text_reduce),
+                articles = reduce,
+                onClickArticle = onNavigateToDetailArticle
+            )
+        }
     }
 }
 
 @Composable
 fun ColorScheme.isLight() = this.background.luminance() > 0.5
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    RebageTheme3 {
+        HomeScreen(
+            snackbarHostState = remember { SnackbarHostState() },
+            usernameState = MutableStateFlow("Tubagus"),
+            allArticlesState = MutableStateFlow(emptyList()),
+            articlesReuse = MutableStateFlow(emptyList()),
+            articlesReduce = MutableStateFlow(emptyList()),
+            statsState = MutableStateFlow(emptyList()),
+            onNavigateToDetailArticle = { }
+        )
+    }
+}
--- app/src/main/java/trashissue/rebage/presentation/home/HomeViewModel.kt
+++ app/src/main/java/trashissue/rebage/presentation/home/HomeViewModel.kt
@@ -4,25 +4,101 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.stateIn
-import trashissue.rebage.domain.model.Result
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import trashissue.rebage.domain.model.Article
+import trashissue.rebage.domain.model.DetectionStatistic
 import trashissue.rebage.domain.usecase.GetArticlesUseCase
+import trashissue.rebage.domain.usecase.GetDetectionsStatisticUseCase
+import trashissue.rebage.domain.usecase.GetUserUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    getArticlesUseCase: GetArticlesUseCase,
-    dispatcher: CoroutineDispatcher
+    getUserUseCase: GetUserUseCase,
+    private val getArticlesUseCase: GetArticlesUseCase,
+    private val getDetectionsStatisticUseCase: GetDetectionsStatisticUseCase,
+    private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
-    val allArticles = getArticlesUseCase()
-        .flowOn(dispatcher)
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), Result.Empty)
-    val handicrafts = getArticlesUseCase(category = "reuse")
-        .flowOn(dispatcher)
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), Result.Empty)
-    val reduce = getArticlesUseCase(category = "reduce")
-        .flowOn(dispatcher)
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), Result.Empty)
+    val username = getUserUseCase()
+        .map { it?.name }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null)
+
+    private val _stats = MutableStateFlow(emptyList<DetectionStatistic>())
+    val stats = _stats.asStateFlow()
+
+    private val _allArticles = MutableStateFlow(emptyList<Article>())
+    val allArticles = _allArticles.asStateFlow()
+
+    private val _articlesReduce = MutableStateFlow(emptyList<Article>())
+    val articlesReduce = _articlesReduce.asStateFlow()
+
+    private val _articleReuse = MutableStateFlow(emptyList<Article>())
+    val articleReuse = _articleReuse.asStateFlow()
+
+    private val _loading = MutableStateFlow(false)
+    val loading = _loading.asStateFlow()
+
+    private val _snackbar = MutableSharedFlow<String>()
+    val snackbar = _snackbar.asSharedFlow()
+
+    init {
+        loadDetectionsStatistic()
+        loadAllArticles()
+        loadArticlesReduce()
+        loadArticlesReuse()
+    }
+
+    private fun loadDetectionsStatistic() {
+        viewModelScope.launch(dispatcher) {
+            getDetectionsStatisticUseCase()
+                .onSuccess { stats ->
+                    _stats.value = stats
+                }
+                .onFailure { e ->
+                    Timber.e(e)
+                    _snackbar.emit("Failed to load articles about reduce")
+                }
+        }
+    }
+
+    private fun loadAllArticles() {
+        viewModelScope.launch(dispatcher) {
+            getArticlesUseCase()
+                .onSuccess { articles ->
+                    _allArticles.value = articles
+                }
+                .onFailure { e ->
+                    Timber.e(e)
+                    _snackbar.emit("Failed to load articles")
+                }
+        }
+    }
+
+    private fun loadArticlesReduce() {
+        viewModelScope.launch(dispatcher) {
+            getArticlesUseCase(category = "Reduce")
+                .onSuccess { articles ->
+                    _articlesReduce.value = articles
+                }
+                .onFailure { e ->
+                    Timber.e(e)
+                    _snackbar.emit("Failed to load articles about reduce")
+                }
+        }
+    }
+
+    private fun loadArticlesReuse() {
+        viewModelScope.launch(dispatcher) {
+            getArticlesUseCase(category = "Reuse")
+                .onSuccess { articles ->
+                    _articleReuse.value = articles
+                }
+                .onFailure { e ->
+                    Timber.e(e)
+                    _snackbar.emit("Failed to load articles about reuse")
+                }
+        }
+    }
 }
