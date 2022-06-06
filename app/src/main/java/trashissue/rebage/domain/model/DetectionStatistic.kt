e2bb9f90d69a1ea92101a15894122e723cf0b967
bagus2x <dutapanggulu@gmail.com>
2022-06-06 09:46:03 +0700

    add annotation to avoid obfuscation

--- app/src/main/java/trashissue/rebage/domain/model/DetectionStatistic.kt
+++ app/src/main/java/trashissue/rebage/domain/model/DetectionStatistic.kt
@@ -3,6 +3,8 @@ package trashissue.rebage.domain.model
 import com.google.gson.annotations.SerializedName
 
 data class DetectionStatistic(
+    @field:SerializedName("label")
     val label: String,
+    @field:SerializedName("total")
     val total: Int
 )
