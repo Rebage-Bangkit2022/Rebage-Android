3f889a536b35d30320cb44cbe37e655691ad5098
bagus2x <dutapanggulu@gmail.com>
2022-06-07 11:24:40 +0700

    add crossfade transition

--- app/src/main/java/trashissue/rebage/presentation/article/component/Photos.kt
+++ app/src/main/java/trashissue/rebage/presentation/article/component/Photos.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
@@ -22,15 +25,21 @@ fun Photos(
     modifier: Modifier = Modifier,
     photos: List<String>
 ) {
+    val photos4 = remember(photos) { photos.take(4) }
+
     Box(modifier = modifier) {
         val pagerState = rememberPagerState()
+
         HorizontalPager(
             modifier = Modifier.fillMaxSize(),
-            count = photos.size,
+            count = photos4.size,
             state = pagerState
         ) { index ->
             AsyncImage(
-                model = photos[index],
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(photos4[index])
+                    .crossfade(500)
+                    .build(),
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop
