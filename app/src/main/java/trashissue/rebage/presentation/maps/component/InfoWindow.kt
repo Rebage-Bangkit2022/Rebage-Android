500e3570ee6c235cf71842ca1b7daf7823873c53
bagus2x <dutapanggulu@gmail.com>
2022-06-08 16:45:42 +0700

    custom InfoWindow for google map

---
+++ app/src/main/java/trashissue/rebage/presentation/maps/component/InfoWindow.kt
@@ -0,0 +1,75 @@
+package trashissue.rebage.presentation.maps.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InfoWindow(
+    modifier: Modifier = Modifier,
+    name: String,
+    phoneNumber: String?,
+    vicinity: String,
+    onClickButtonCheckLocation: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp,
+                pressedElevation = 8.dp
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = name,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
+                )
+                if (phoneNumber != null) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 4.dp),
+                        text = phoneNumber,
+                        maxLines = 2,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 4.dp),
+                    text = vicinity,
+                    maxLines = 2,
+                    style = MaterialTheme.typography.titleSmall
+                )
+            }
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onClickButtonCheckLocation
+        ) {
+            Text(text = "Open  Via Google Maps")
+        }
+    }
+}
