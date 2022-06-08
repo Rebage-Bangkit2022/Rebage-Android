5b0558205c8bee503bf10af6e962732acc1baf45
bagus2x <dutapanggulu@gmail.com>
2022-06-08 16:37:12 +0700

    rename class from GoogleMapResponse to NearbySearchResponse

--- app/src/main/java/trashissue/rebage/data/remote/service/GoogleMapService.kt
+++ app/src/main/java/trashissue/rebage/data/remote/service/GoogleMapService.kt
@@ -4,11 +4,11 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 import trashissue.rebage.BuildConfig
-import trashissue.rebage.data.remote.payload.GoogleMapResponse
+import trashissue.rebage.data.remote.payload.NearbySearchResponse
 
 interface GoogleMapService {
 
-    @GET("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${BuildConfig.GOOGLE_MAPS_SEARCH_KEY}")
+    @GET("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${BuildConfig.GOOGLE_MAPS_WEB_KEY}")
     suspend fun getNearby(
         @Query("location")
         location: String?,
@@ -18,5 +18,5 @@ interface GoogleMapService {
         type: String?,
         @Query("keyword")
         keyword: String?
-    ): Response<GoogleMapResponse>
+    ): Response<NearbySearchResponse>
 }
