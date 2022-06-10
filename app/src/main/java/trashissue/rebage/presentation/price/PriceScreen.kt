e38da6a90bb92e828c24d82ed609377e9f65fa5a
bagus2x <dutapanggulu@gmail.com>
2022-06-10 14:15:17 +0700

    create PriceScreen and connect with domain layer

--- app/src/main/java/trashissue/rebage/presentation/price/PriceScreen.kt
+++ app/src/main/java/trashissue/rebage/presentation/price/PriceScreen.kt
@@ -1,23 +1,50 @@
 package trashissue.rebage.presentation.price
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Map
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
-import timber.log.Timber
-import trashissue.rebage.presentation.common.component.shimmer
+import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import kotlinx.coroutines.flow.StateFlow
+import trashissue.rebage.R
+import trashissue.rebage.domain.model.Garbage
+import trashissue.rebage.presentation.common.component.AnimatedCounter
+import trashissue.rebage.presentation.common.component.Dropdown
+import trashissue.rebage.presentation.main.Route
+
+@Composable
+fun PriceScreen(
+    navController: NavHostController,
+    viewModel: PriceViewModel = hiltViewModel(),
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    PriceScreen(
+        snackbarHostState = snackbarHostState,
+        garbageState = viewModel.garbage,
+        onNavigateToGarbageBank = {
+            navController.navigate(Route.GarbageBank())
+        }
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PriceScreen(
-    viewModel: PriceViewModel = hiltViewModel()
+    snackbarHostState: SnackbarHostState,
+    garbageState: StateFlow<List<Garbage>>,
+    onNavigateToGarbageBank: () -> Unit
 ) {
     Scaffold(
         modifier = Modifier
@@ -26,45 +53,92 @@ fun PriceScreen(
         topBar = {
             SmallTopAppBar(
                 title = {
-                    Text(text = "Price")
+                    Text(text = stringResource(R.string.text_price))
                 }
             )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = onNavigateToGarbageBank,
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.surface,
+                text = {
+                    Text(text = stringResource(R.string.text_garbage_bank))
+                },
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Map,
+                        contentDescription = null
+                    )
+                }
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
     ) { innerPadding ->
-        LaunchedEffect(Unit) {
-            withContext(Dispatchers.IO) {
-                repeat(1000) {
-                    delay(1000)
-                    Timber.i("HELLO $it")
-                }
-            }
-        }
-
         Column(
             modifier = Modifier
+                .statusBarsPadding()
                 .padding(innerPadding)
-                .background(
-                    CardDefaults
-                        .cardColors()
-                        .containerColor(enabled = true).value
-                )
         ) {
-            var enabled by remember {
-                mutableStateOf(false)
-            }
-
-            Button(
-                onClick = {
-                    enabled = !enabled
-                }
-            ) {
-                Text(text = "Hello")
-            }
-            Box(
+            Card(
                 modifier = Modifier
-                    .size(240.dp)
-                    .shimmer(enabled, CircleShape)
-            )
+                    .padding(16.dp)
+                    .animateContentSize()
+            ) {
+                Column(
+                    modifier = Modifier.padding(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    val garbage by garbageState.collectAsState()
+                    val options = rememberSaveable(garbage) { garbage.map { it.name } }
+                    var selected by rememberSaveable { mutableStateOf<Garbage?>(null) }
+
+                    Dropdown(
+                        options = options,
+                        value = selected?.name ?: "",
+                        onValueChange = { value ->
+                            selected = garbage.find { it.name == value }
+                        }
+                    )
+
+                    val image = selected?.image
+                    val name = selected?.name
+                    val price = selected?.price
+
+                    if (image != null && name != null && price != null) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(12.dp),
+                        ) {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .size(height = 120.dp, width = 100.dp)
+                                    .clip(MaterialTheme.shapes.small),
+                                model = image,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop
+                            )
+                            var totalItem by rememberSaveable { mutableStateOf(1) }
+
+                            AnimatedCounter(
+                                modifier = Modifier.align(Alignment.Bottom),
+                                value = totalItem,
+                                onClickDecrement = { if (totalItem != 1) totalItem-- },
+                                onClickIncrement = { totalItem++ }
+                            )
+                            Spacer(modifier = Modifier.weight(1F))
+                            Text(
+                                modifier = Modifier.align(Alignment.Bottom),
+                                text = "Rp${price * totalItem}",
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/trashissue/rebage/presentation/price/PriceViewModel.kt
+++ app/src/main/java/trashissue/rebage/presentation/price/PriceViewModel.kt
@@ -3,21 +3,47 @@ package trashissue.rebage.presentation.price
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
+import trashissue.rebage.domain.model.Garbage
+import trashissue.rebage.domain.usecase.GetGarbageUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class PriceViewModel @Inject constructor() : ViewModel() {
-    val state= MutableStateFlow(0)
+class PriceViewModel @Inject constructor(
+    private val getGarbageUseCase: GetGarbageUseCase,
+    private val dispatcher: CoroutineDispatcher
+) : ViewModel() {
+    private val _garbage = MutableStateFlow<List<Garbage>>(emptyList())
+    val garbage = _garbage.asStateFlow()
+
+    private val _snackbar = MutableSharedFlow<String>()
+    val snackbar = _snackbar.asSharedFlow()
+
+    private val _loading = MutableStateFlow(false)
+    val loading = MutableStateFlow(false)
 
     init {
-        viewModelScope.launch {
-            repeat(1000) {
-                delay(1000)
-                state.value = it
-            }
+        loadGarbage()
+    }
+
+    private fun loadGarbage() {
+        viewModelScope.launch(dispatcher) {
+            _loading.value = true
+            getGarbageUseCase()
+                .onSuccess { garbage ->
+                    _garbage.value = garbage
+                }
+                .onFailure { e ->
+                    Timber.e(e)
+                    _snackbar.emit(e.message ?: "Failed to load garbage")
+                }
+            _loading.value = false
         }
     }
 }
