974d47f0ebaf3eef1dfdf4f1a77b2fa6f74fc05a
bagus2x <dutapanggulu@gmail.com>
2022-06-10 14:11:57 +0700

    add parcelize

--- app/src/main/java/trashissue/rebage/domain/model/Garbage.kt
+++ app/src/main/java/trashissue/rebage/domain/model/Garbage.kt
@@ -1,8 +1,12 @@
 package trashissue.rebage.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Garbage(
     val id: Int,
     val name: String,
     val price: Int,
     val image: String
-)
+) : Parcelable
