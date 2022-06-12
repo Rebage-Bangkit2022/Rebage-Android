d204eddbc0cc7f827a3edb20d51db16fb5a2589f
bagus2x <dutapanggulu@gmail.com>
2022-06-13 06:30:19 +0700

    provide provideFusedLocalProviderClient

--- app/src/main/java/trashissue/rebage/di/CommonModule.kt
+++ app/src/main/java/trashissue/rebage/di/CommonModule.kt
@@ -1,8 +1,12 @@
 package trashissue.rebage.di
 
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -16,5 +20,11 @@ object CommonModule {
     @Singleton
     fun provideDispatchers(): CoroutineDispatcher {
         return Dispatchers.IO
+    }
+
+    @Provides
+    @Singleton
+    fun provideFusedLocalProviderClient(@ApplicationContext context: Context): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(context)
     }
 }
