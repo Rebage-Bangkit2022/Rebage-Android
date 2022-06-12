ddf29bc09cc30e69ddb7222ddddd925a61f40fe2
bagus2x <dutapanggulu@gmail.com>
2022-06-13 06:41:39 +0700

    create extension function with flow to observe location change

--- app/src/main/java/trashissue/rebage/presentation/common/FusedLocationProviderClient.kt
+++ app/src/main/java/trashissue/rebage/presentation/common/FusedLocationProviderClient.kt
@@ -1,8 +1,15 @@
 package trashissue.rebage.presentation.common
 
+import android.annotation.SuppressLint
 import android.location.Location
+import android.os.Looper
 import androidx.annotation.RequiresPermission
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -18,3 +25,38 @@ suspend fun FusedLocationProviderClient.awaitLastLocation(): Location? =
     }
 
 
+// Send location updates to the consumer
+@SuppressLint("MissingPermission")
+fun FusedLocationProviderClient.locationFlow() = callbackFlow<Location> {
+    // A new Flow is created. This code executes in a coroutine!
+
+    // 1. Create callback and add elements into the flow
+    val callback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            result ?: return // Ignore null responses
+            for (location in result.locations) {
+                try {
+                    trySend(location).isSuccess // Send location to the flow
+                } catch (t: Throwable) {
+                    // Location couldn't be sent to the flow
+                }
+            }
+        }
+    }
+
+    // 2. Register the callback to get location updates by calling requestLocationUpdates
+    requestLocationUpdates(
+        LocationRequest.create(),
+        callback,
+        Looper.getMainLooper()
+    ).addOnFailureListener { e ->
+        close(e) // in case of error, close the Flow
+    }
+
+    // 3. Wait for the consumer to cancel the coroutine and unregister
+    // the callback. This suspends the coroutine until the Flow is closed.
+    awaitClose {
+        // Clean up code goes here
+        removeLocationUpdates(callback)
+    }
+}
