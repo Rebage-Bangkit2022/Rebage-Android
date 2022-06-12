fb3c959b474a7f1b2c5b10d0f9fd0d17f79102dc
bagus2x <dutapanggulu@gmail.com>
2022-06-13 06:34:52 +0700

    add method "delete"

--- app/src/main/java/trashissue/rebage/data/local/DetectionLocalDataSource.kt
+++ app/src/main/java/trashissue/rebage/data/local/DetectionLocalDataSource.kt
@@ -8,19 +8,23 @@ class DetectionLocalDataSource(
     private val detectionDao: DetectionDao
 ) {
 
-    suspend fun saveDetection(detection: DetectionEntity) {
-        detectionDao.saveDetection(detection)
+    suspend fun save(detection: DetectionEntity) {
+        detectionDao.save(detection)
     }
 
-    suspend fun saveDetections(detections: List<DetectionEntity>) {
-        detectionDao.saveDetections(detections)
+    suspend fun save(detections: List<DetectionEntity>) {
+        detectionDao.save(detections)
     }
 
     fun getDetections(): Flow<List<DetectionEntity>> {
         return detectionDao.getDetections()
     }
 
-    suspend fun deleteDetection(detectionEntity: DetectionEntity) {
-        detectionDao.deleteDetection(detectionEntity)
+    suspend fun delete(detectionEntity: DetectionEntity) {
+        detectionDao.delete(detectionEntity)
+    }
+
+    suspend fun delete() {
+        detectionDao.delete()
     }
 }
