8fd01305e9a17e6d5e774d46cd348e4369355d1a
bagus2x <dutapanggulu@gmail.com>
2022-06-13 07:05:49 +0700

    add snackbar host state

--- app/src/main/java/trashissue/rebage/presentation/editprofile/EditProfileScreen.kt
+++ app/src/main/java/trashissue/rebage/presentation/editprofile/EditProfileScreen.kt
@@ -56,6 +56,9 @@ fun EditProfileScreen(
                     }
                 }
             )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
     ) { innerPadding ->
         Box(
